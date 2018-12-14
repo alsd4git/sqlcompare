5e94b29721abbb345e10fcccd8d4e103f61e2e40
Alessandro Digilio <alessandro.digilio@hotmail.it>
2018-12-14 11:12:56 +0100

    first commit, updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # sqlcompare
 simple shell script to compare two SQL create tables
+
+
+to start:
+```sh
+git clone https://github.com/alsd4git/sqlcompare
+cd dotfiles
+chmod a+x SqlCompare.sh
+```
+
+I usually just use it with 
+```sh
+sh SqlCompare.sh old_db.sql new_db.sql 1
+```
+
+this script was written on a windows machine using git bash, but it should work on plain bash with no problems, you can use ```"SqlCompare.sh -h``` to get a simple help
---
+++ SqlCompare.sh
@@ -0,0 +1,73 @@
+usage="`basename $0` [-h] [oldFile newFile diffType] -- program to clean and compare two SQL create table
+
+where:
+	-h  show this help text
+	oldFile: name of the old SQL create table
+	newFile: name of the new SQL create table
+	diffType: value between 1 and 4 where:
+		1) use icdiff (python) if installed, can get with 'pip install git+https://github.com/jeffkaufman/icdiff.git'
+		2) use meld (windows) if used with git bash, you need windows version of meld installed in x86 program folder
+		3) use standard diff with default coloring
+		4) use standard diff with some grep colors
+"
+
+if [ "$1" == "-h" ]; then
+  echo "$usage"
+  exit 0
+fi
+
+if [ -z "$1" ]
+  then
+    echo "Warn: missing old file name, will use 'old_DB.sql'"
+	#$1='old_DB.sql'
+	#to edit passed value you need to set them all
+	set -- 'old_DB.sql' "${@:2:3}"
+fi
+if [ -z "$2" ]
+  then
+    echo "Warn: missing new file name, will use 'new_DB.sql'"
+	#$2='new_DB.sql'
+	#to edit passed value you need to set them all
+	set -- "${@:1}" 'new_DB.sql' "${@:3}"
+fi
+
+if [ ! -f "$1" ]; then
+    echo "File $1 not found! use \"`basename $0` -h\" for script usage"
+	exit 1
+fi
+
+if [ ! -f "$2" ]; then
+    echo "File $2 not found! use \"`basename $0` -h\" for script usage"
+	exit 1
+fi
+
+#cleaning sql create table here
+cat "$1" | grep -v '^/\*\|^$\|^-- \|^  CONSTRAINT' | sed '/ENGINE=/c\);\n' | sed 's/ COMMENT.*$//' > clean_"$1"
+cat "$2" | grep -v '^/\*\|^$\|^-- \|^  CONSTRAINT' | sed '/ENGINE=/c\);\n' | sed 's/ COMMENT.*$//' > clean_"$2"
+
+#sed '/ENGINE=/c\);\n' replace any line that contains '/ENGINE=' with ');\n'
+#sed 's/ COMMENT.*$//' removed everything in the line after ' COMMENT' with that string included
+#'\|' is a separator to be used as "OR" 
+
+case $3 in
+1)
+	echo '1 - will use icdiff (python)'
+	icdiff clean_"$1" clean_"$2" --cols=150 -W
+	;;
+2)
+	echo '2 - will use meld (windows)'
+	'C:\Program Files (x86)\Meld\meld.exe' clean_"$1" clean_"$2" &
+	;;
+3)
+	echo '3 - will use basic diff'
+	diff clean_"$1" clean_"$2" --color=always
+	;;
+4)
+	echo '4 - will use diff with colored grep'
+	diff -y clean_"$1" clean_"$2" | GREP_COLOR='01;32' egrep --color=always '.*>.*|$' | GREP_COLOR='01;31' egrep --color=always '.*<.*|$' | GREP_COLOR='01;36' egrep --color=always '.*\|.*|$' | less -R
+	;;
+*)
+	echo 'no option/invalid option provided will use diff with colored grep'
+	diff -y clean_"$1" clean_"$2" | GREP_COLOR='01;32' egrep --color=always '.*>.*|$' | GREP_COLOR='01;31' egrep --color=always '.*<.*|$' | GREP_COLOR='01;36' egrep --color=always '.*\|.*|$' | less -R
+	;;
+esac
